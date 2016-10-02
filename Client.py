fbb3d44a0f4e62f387ca1d7b31fdf93bb0a8356f
刘  方瑞 <mpskex@163.com>
2016-10-02 17:38:50 +0800

    Init git

---
+++ Client.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+##coding=utf-8
+'''
+Author:     mpsk
+Date:       2016-10-02
+Function:   Client for 
+            Remote Connection via Proxy Server in TCP/IP Socket
+Version:    1.0.1
+'''
+import socket
+import time
+
+
+class target(object):
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+class session(object):
+    def __init__(self,target):
+        self.target = target
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def connect(self):
+        print 'Connecting ' + self.target.ip + ':' +str(self.target.port)
+        self.sock.connect((self.target.ip, self.target.port))
+    def establish(self):
+        self.sock.send('123456')
+        if(self.sock.recv(20) == "Welcome to server!"): print "Connection established successfully!"
+        else: print "failed to connect!"
+    def command(self):
+            cmd=raw_input("Please input cmd:")
+            if(cmd=='quit!'):
+                print 'Stopped!' 
+            else:
+                self.sock.sendall(cmd)      
+                data=self.sock.recv(1024)     
+                print data
+    def shutserver(self):self.sock.send('quit!')
+    def close(self): self.sock.close
+
+ip = raw_input("Please input IP Address:")
+port = raw_input("Please input Port:")
+b = session(target(ip, port))
+while(True):
+    b.connect()
+    b.command()
+    b = session(target(ip, port))
+b.close()
+'''
+##Legacy TCP/IP
+
+def init():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return sock
+
+s = init()
+s.connect(('localhost',65532))
+
+time.sleep(2)
+s.send('exit')
+print s.recv(20)
+s.close
+'''
---
+++ Server.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+##coding = utf-8
+'''
+Author:     mpsk
+Date:       2016-10-02
+Function:   Server for 
+            Remote Connection via Proxy Server in TCP/IP Socket
+Version:    1.0.1
+'''
+import socket
+import time
+import SocketServer
+import commands
+import os
+
+class target(object):
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+class server(object):
+    def __init__(self, target):
+        self.target = target
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def bind(self):
+        self.sock.bind((self.target.ip, self.target.port))
+    def listen(self):
+        self.sock.listen(5)
+        while (True):
+            conn,addr=self.sock.accept() 
+            print'Connected by',addr    
+            try:
+                conn.settimeout(300)
+                data=conn.recv(1024)
+                if(data=='quit!'):break
+                else:##os.system(cmd) 
+                
+                    cmd_status,cmd_result=commands.getstatusoutput(data)   
+                if len(cmd_result.strip())==0:   
+                    conn.sendall('Done.')
+                else:
+                    conn.sendall(cmd_result)
+                
+            except socket.error:
+                print 'socket time out!'
+    def close(self):self.sock.close()
+
+ip = raw_input("Please input IP Address:")
+port = raw_input("Please input Port:")
+a = server(target(ip, port))
+a.bind()
+a.listen()
+a.close()
+'''
+def init():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost',65532))
+    return sock
+
+s = init()
+s.listen(5)
+while(True):
+    conn,addr = s.accept()
+    print 'Connection established! Client address is',addr
+    try:
+        conn.settimeout(2)
+        buff = conn.recv(8)
+        if(buff=='123456'):
+            conn.send("Welcome to server!")
+        elif(buff=='exit'):
+            break
+        else:
+            conn.send("Request denied!")
+    except socket.error:
+        print 'Socket time out!'
+    conn.close()
+'''
